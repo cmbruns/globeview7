71c00bf550dcae8a80cc107c4e9fd034bfab3f2c
Christopher Bruns <cmbruns@rotatingpenguin.com>
2022-09-17 14:17:37 -0700

    Don't accept dragging outside projection bounds.

--- gv/geocanvas.py
+++ gv/geocanvas.py
@@ -66,8 +66,8 @@ class GeoCanvas(QtOpenGLWidgets.QOpenGLWidget):
                 try:
                     dobq = self.view_state._projection.dobq_for_dnmc(dnmc, p_nmc)
                 except RuntimeError:
+                    self.previous_mouse = None
                     return  # TODO: handle outside cases in projection
-                print(f"dobq: {dobq}")
                 # Compute center latitude shift from obq shift
                 px, py, pz = p_obq
                 xy2 = px**2 + py**2
@@ -75,7 +75,6 @@ class GeoCanvas(QtOpenGLWidgets.QOpenGLWidget):
                 dlat = numpy.dot(dobq, [-px * pz / sxy2, -py * pz / sxy2, sxy2])
                 #
                 decf = self.view_state.ecf_J_obq @ dobq
-                print(f"decf: {decf}")
                 px, py, pz = p_ecf
                 xy2 = px**2 + py**2
                 sxy2 = xy2 ** 0.5
--- gv/projection.py
+++ gv/projection.py
@@ -1,6 +1,6 @@
 import abc
 import enum
-import math
+from math import cos, radians, sin
 
 import numpy
 
@@ -73,19 +73,21 @@ class WGS84Projection(DisplayProjection):
     def obq_for_nmc(p_nmc: NMCPoint) -> OBQPoint:
         p_prj = p_nmc  # Radians from normalized units
         result = OBQPoint((
-            math.cos(p_prj[0]) * math.cos(p_prj[1]),
-            math.sin(p_prj[0]) * math.cos(p_prj[1]),
-            math.sin(p_prj[1]),
+            cos(p_prj[0]) * cos(p_prj[1]),
+            sin(p_prj[0]) * cos(p_prj[1]),
+            sin(p_prj[1]),
         ))
         return result
 
     @staticmethod
     def dobq_for_dnmc(dnmc, p_nmc: NMCPoint):
         p_prj = p_nmc  # Radians are normalized units
-        c0 = math.cos(p_prj[0])
-        s0 = math.sin(p_prj[0])
-        c1 = math.cos(p_prj[1])
-        s1 = math.sin(p_prj[1])
+        if abs(p_prj[1]) > radians(90):
+            raise RuntimeError
+        c0 = cos(p_prj[0])
+        s0 = sin(p_prj[0])
+        c1 = cos(p_prj[1])
+        s1 = sin(p_prj[1])
         obq_J_wgs84prj = numpy.array([
             [-s0 * c1, -c0 * s1],
             [c0 * c1, -s0 * s1],
