d7f9af4955781b12e6fa2bccabad2784858de82b
Christopher Bruns <cmbruns@rotatingpenguin.com>
2022-09-28 08:21:54 -0700

    Refactor projection boundaries to use new VertexBuffer class.

--- gv/projection.py
+++ gv/projection.py
@@ -8,6 +8,7 @@ from OpenGL import GL
 from OpenGL.GL.shaders import compileShader, compileProgram
 
 from gv.frame import NMCPoint, OBQPoint, FramedPoint
+from gv.vertex_buffer import VertexBuffer
 
 
 class Projection(enum.IntEnum):
@@ -53,26 +54,16 @@ class OrthographicProjection(DisplayProjection):
 
     def __init__(self):
         super().__init__()
-        self.boundary_vao = None
-        self.boundary_vbo = None
         self.boundary_shader = None
         r = 1.0
         cverts = 100
         verts = [[r * sin(2 * i * pi / cverts), r * cos(2 * i * pi / cverts), 1] for i in range(cverts)]
-        self.boundary_vertices = numpy.array(verts, dtype=numpy.float32)
+        self.boundary_vertices = VertexBuffer(verts)
 
     def initialize_gl(self):
-        self.boundary_vao = GL.glGenVertexArrays(1)
-        GL.glBindVertexArray(self.boundary_vao)
-        # return  # OK
-        self.boundary_vbo = GL.glGenBuffers(1)
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.boundary_vbo)
-        # return  # OK
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.boundary_vertices, GL.GL_STATIC_DRAW)
-        # return  # too late
-        GL.glEnableVertexAttribArray(0)
-        GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 0, None)
-        # return  # too late
+        if self.boundary_shader is not None:
+            return
+        self.boundary_vertices.initialize_opengl()
         self.boundary_shader = compileProgram(
             compileShader(inspect.cleandoc("""
                 #version 430
@@ -98,23 +89,23 @@ class OrthographicProjection(DisplayProjection):
         )
 
     def draw_boundary(self, context):
-        if self.boundary_vao is None:
+        if self.boundary_shader is None:
             self.initialize_gl()
-        GL.glBindVertexArray(self.boundary_vao)
-        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
-        GL.glEnable(GL.GL_BLEND)
-        GL.glEnable(GL.GL_LINE_SMOOTH)
-        GL.glLineWidth(1)
-        GL.glUseProgram(self.boundary_shader)
-        GL.glUniformMatrix3fv(1, 1, True, context.ndc_X_nmc)
-        # GL.glPatchParameteri(GL.GL_PATCH_VERTICES, 2)  # TODO: more tessellation
-        GL.glDrawArrays(GL.GL_LINE_LOOP, 0, len(self.boundary_vertices))
+        with self.boundary_vertices as bv:
+            GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+            GL.glEnable(GL.GL_BLEND)
+            GL.glEnable(GL.GL_LINE_SMOOTH)
+            GL.glLineWidth(1)
+            GL.glUseProgram(self.boundary_shader)
+            GL.glUniformMatrix3fv(1, 1, True, context.ndc_X_nmc)
+            # GL.glPatchParameteri(GL.GL_PATCH_VERTICES, 2)  # TODO: more tessellation
+            GL.glDrawArrays(GL.GL_LINE_LOOP, 0, len(bv))
 
     def fill_boundary(self, context):
-        if self.boundary_vao is None:
+        if self.boundary_shader is None:
             self.initialize_gl()
-        GL.glBindVertexArray(self.boundary_vao)
-        GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, len(self.boundary_vertices))
+        with self.boundary_vertices as bv:
+            GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, len(bv))
 
     @staticmethod
     def dobq_for_dnmc(dnmc, p_nmc: NMCPoint):
@@ -165,10 +156,8 @@ class WGS84Projection(DisplayProjection):
 
     def __init__(self):
         super().__init__()
-        self.boundary_vao = None
-        self.boundary_vbo = None
         self.boundary_shader = None
-        self.boundary_vertices = numpy.array([
+        self.boundary_vertices = VertexBuffer([
             # Clockwise, like shapefiles
             # Normalized map coordinates
             # For use in triangle fans, it's important for the first vertex to be non-infinite.
@@ -176,17 +165,11 @@ class WGS84Projection(DisplayProjection):
             [-1, 0, 0],  # west infinity
             [0, radians(90), 1],  # center of northern boundary
             [1, 0, 0],  # east infinity
-        ], dtype=numpy.float32)
+        ])
 
     def initialize_gl(self):
-        if self.boundary_vao is None:
-            self.boundary_vao = GL.glGenVertexArrays(1)
-            GL.glBindVertexArray(self.boundary_vao)
-            self.boundary_vbo = GL.glGenBuffers(1)
-            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.boundary_vbo)
-            GL.glBufferData(GL.GL_ARRAY_BUFFER, self.boundary_vertices, GL.GL_STATIC_DRAW)
-            GL.glEnableVertexAttribArray(0)
-            GL.glVertexAttribPointer(0, 3, GL.GL_FLOAT, False, 0, None)
+        if self.boundary_shader is None:
+            self.boundary_vertices.initialize_opengl()
             self.boundary_shader = compileProgram(
                 compileShader(inspect.cleandoc("""
                     #version 430
@@ -212,23 +195,23 @@ class WGS84Projection(DisplayProjection):
             )
 
     def draw_boundary(self, context):
-        if self.boundary_vao is None:
+        if self.boundary_shader is None:
             self.initialize_gl()
-        GL.glBindVertexArray(self.boundary_vao)
-        # TODO: common gl state for all?
-        GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
-        GL.glEnable(GL.GL_BLEND)
-        GL.glEnable(GL.GL_LINE_SMOOTH)
-        GL.glLineWidth(1)
-        GL.glUseProgram(self.boundary_shader)
-        GL.glUniformMatrix3fv(1, 1, True, context.ndc_X_nmc)
-        GL.glDrawArrays(GL.GL_LINE_LOOP, 0, len(self.boundary_vertices))
+        with self.boundary_vertices as bv:
+            # TODO: common gl state for all?
+            GL.glBlendFunc(GL.GL_SRC_ALPHA, GL.GL_ONE_MINUS_SRC_ALPHA)
+            GL.glEnable(GL.GL_BLEND)
+            GL.glEnable(GL.GL_LINE_SMOOTH)
+            GL.glLineWidth(1)
+            GL.glUseProgram(self.boundary_shader)
+            GL.glUniformMatrix3fv(1, 1, True, context.ndc_X_nmc)
+            GL.glDrawArrays(GL.GL_LINE_LOOP, 0, len(self.boundary_vertices))
 
     def fill_boundary(self, context):
-        if self.boundary_vao is None:
+        if self.boundary_shader is None:
             self.initialize_gl()
-        GL.glBindVertexArray(self.boundary_vao)
-        GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, len(self.boundary_vertices))
+        with self.boundary_vertices as bv:
+            GL.glDrawArrays(GL.GL_TRIANGLE_FAN, 0, len(self.boundary_vertices))
 
     @staticmethod
     def obq_for_nmc(p_nmc: NMCPoint) -> OBQPoint:
--- gv/vertex_buffer.py
+++ gv/vertex_buffer.py
@@ -10,20 +10,25 @@ class VertexBuffer(object):
         self.element_count = len(vertices[0])
         self.vertices = numpy.array(vertices, dtype=numpy.float32).flatten()
 
-    def __enter___(self):
+    def __enter__(self):
         if self.vao is None:
             self.initialize_opengl()
         GL.glBindVertexArray(self.vao)
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         GL.glBindVertexArray(0)
 
+    def __len__(self):
+        return self.vertex_count
+
     def initialize_opengl(self):
-        self.vao = GL.glGenVertexArrays(1)
-        GL.glBindVertexArray(self.vao)
-        self.vbo = GL.glGenBuffers(1)
-        GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
-        GL.glBufferData(GL.GL_ARRAY_BUFFER, self.vertices, GL.GL_STATIC_DRAW)
-        GL.glEnableVertexAttribArray(0)
-        GL.glVertexAttribPointer(0, self.element_count, GL.GL_FLOAT, False, 0, None)
-        GL.glBindVertexArray(0)
+        if self.vao is None:
+            self.vao = GL.glGenVertexArrays(1)
+            GL.glBindVertexArray(self.vao)
+            self.vbo = GL.glGenBuffers(1)
+            GL.glBindBuffer(GL.GL_ARRAY_BUFFER, self.vbo)
+            GL.glBufferData(GL.GL_ARRAY_BUFFER, self.vertices, GL.GL_STATIC_DRAW)
+            GL.glEnableVertexAttribArray(0)
+            GL.glVertexAttribPointer(0, self.element_count, GL.GL_FLOAT, False, 0, None)
+            GL.glBindVertexArray(0)
