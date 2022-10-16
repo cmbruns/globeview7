46f29e13723639d280e7f6035ee40a46c219b26a
Christopher Bruns <cmbruns@rotatingpenguin.com>
2022-10-16 16:47:53 -0700

    Set initial projection to orthographic. Don't use cyclic combobox for projection.

--- gv/ui_globeview.py
+++ gv/ui_globeview.py
@@ -16,12 +16,12 @@ from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
     QIcon, QImage, QKeySequence, QLinearGradient,
     QPainter, QPalette, QPixmap, QRadialGradient,
     QTransform)
-from PySide6.QtWidgets import (QApplication, QDoubleSpinBox, QGroupBox, QHBoxLayout,
-    QLabel, QListWidgetItem, QMainWindow, QMenu,
-    QMenuBar, QPushButton, QSizePolicy, QSpacerItem,
-    QSplitter, QStatusBar, QVBoxLayout, QWidget)
+from PySide6.QtWidgets import (QApplication, QComboBox, QDoubleSpinBox, QGroupBox,
+    QHBoxLayout, QLabel, QListWidgetItem, QMainWindow,
+    QMenu, QMenuBar, QPushButton, QSizePolicy,
+    QSpacerItem, QSplitter, QStatusBar, QVBoxLayout,
+    QWidget)
 
-from circular_combobox import CircularCombobox
 from geocanvas import GeoCanvas
 from gv.layer import LayerListWidget
 
@@ -110,7 +110,7 @@ class Ui_MainWindow(object):
         self.groupBox.setObjectName(u"groupBox")
         self.verticalLayout_6 = QVBoxLayout(self.groupBox)
         self.verticalLayout_6.setObjectName(u"verticalLayout_6")
-        self.projectionComboBox = CircularCombobox(self.groupBox)
+        self.projectionComboBox = QComboBox(self.groupBox)
         self.projectionComboBox.addItem("")
         self.projectionComboBox.addItem("")
         self.projectionComboBox.addItem("")
--- gv/view_state.py
+++ gv/view_state.py
@@ -4,7 +4,7 @@ import numpy
 from PySide6 import QtCore
 
 from gv.frame import WGS84Point, WindowPoint
-from gv.projection import EquirectangularProjection
+from gv.projection import OrthographicProjection
 from gv.layer import ILayer
 
 
@@ -17,7 +17,7 @@ class Transform(object):
 class ViewState(QtCore.QObject):
     def __init__(self):
         super().__init__()
-        self._projection = EquirectangularProjection()
+        self._projection = OrthographicProjection()
         self._window_size = [1, 1]
         # TODO: should center be in degrees? Does the principle of best exactness apply here?
         self._center_location = [radians(0), radians(0)]
